5fd9546ca5564ec25f6379ba63c582157f3858b5
hendisantika <hendisantika@yahoo.co.id>
2019-03-01 06:44:16 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootticksample/SpringbootTickSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootticksample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTickSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTickSampleApplication.class, args);
+    }
+
+}
