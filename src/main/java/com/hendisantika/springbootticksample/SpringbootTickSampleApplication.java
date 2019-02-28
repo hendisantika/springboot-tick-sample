c61019e014cb29db39e900a3b075327bb6fbce60
hendisantika <hendisantika@yahoo.co.id>
2019-03-01 06:50:15 +0700

    #8 - Modify Main Class

--- src/main/java/com/hendisantika/springbootticksample/SpringbootTickSampleApplication.java
+++ src/main/java/com/hendisantika/springbootticksample/SpringbootTickSampleApplication.java
@@ -1,13 +1,37 @@
 package com.hendisantika.springbootticksample;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringbootTickSampleApplication {
 
+    private static Logger logger = LogManager.getLogger(SpringbootTickSampleApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootTickSampleApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                logger.info(beanName);
+            }
+
+        };
     }
 
 }
